e0d060c1c5fa38a586dda03056aca35c2589a369
TAHA RAHIMY <rahimytaha9@gmail.com>
2025-12-18 11:44:12 +0330

    auth controller added

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,7 +1,18 @@
-import { Controller } from "@nestjs/common";
+import { Body, Controller, Post } from "@nestjs/common";
+import { SignInDto } from "./dto/signIn.dto";
+import { AuthService } from "./auth.service";
+import { signUpDto } from "./dto/signUp.dto";
 
 
 @Controller()
 export class AuthController{
-    
+    constructor(private authService:AuthService){}
+    @Post("/signIn")
+    async signIn (@Body() dto:SignInDto) {
+        return this.authService.signIn(dto)
+    }
+    @Post("/signUp")
+    async signUp (@Body() dto:signUpDto) {
+        return this.authService.signUp(dto)
+    }
 }
