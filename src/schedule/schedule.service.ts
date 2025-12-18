dc14ed37cf04b1076b8b72d26a7f7a3327c9d50a
TAHA RAHIMY <rahimytaha9@gmail.com>
2025-12-18 22:56:44 +0330

    .

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -2,6 +2,8 @@ import { Injectable } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ScheduleEntity } from './entity/schedule.entity';
 import { Repository } from 'typeorm';
+import { CreateScheduleDto } from './dto/createSchedule.dto';
+import { UpdateScheduleDto } from './dto/updateSchedule.dto';
 
 @Injectable()
 export class ScheduleService {
@@ -13,5 +15,8 @@ export class ScheduleService {
     const data = await this.scheduleRepository.find();
     return data;
   }
+  async create(dto:CreateScheduleDto){}
+  async update(dto:UpdateScheduleDto){}
+  async delete(id:UpdateScheduleDto){}
   
 }
