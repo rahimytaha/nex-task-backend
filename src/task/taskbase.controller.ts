37163b8f146f9a15d620c0de19407148eb2d0887
TAHA RAHIMY <rahimytaha9@gmail.com>
2025-12-19 20:30:18 +0330

    task base controller is done

--- src/task/taskbase.controller.ts
+++ src/task/taskbase.controller.ts
@@ -1,8 +1,17 @@
-import { Body, Controller, Get, Param, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  Param,
+  Post,
+  Put,
+} from '@nestjs/common';
 import { TaskService } from './task.service';
 import { UserPayload } from 'src/commen/decorator/user.decorator';
 import type { TUserPayload } from 'src/commen/type/userPayload';
 import { CreateTaskDto } from './dto/createTask.dto';
+import { UpdateTaskDto } from './dto/updateTask.dto';
 
 @Controller('base')
 export class TaskBaseController {
@@ -21,5 +30,24 @@ export class TaskBaseController {
   @Post('/create')
   create(@UserPayload() { id }: TUserPayload, @Body() dto: CreateTaskDto) {
     return this.taskService.createTask(dto, id);
+  }
+  @Put('/update/:id')
+  update(
+    @UserPayload() { id }: TUserPayload,
+    @Body() dto: UpdateTaskDto,
+    @Param('id') taskId: number,
+  ) {
+    return this.taskService.updateTask(dto, taskId, id);
+  }
+  @Delete('byId/:id')
+  delete(@UserPayload() { id: userId }: TUserPayload, @Param('id') id: number) {
+    return this.taskService.deleteTask(id, userId);
+  }
+  @Get('mine/:scheduleId')
+  mine(
+    @UserPayload() { id: userId }: TUserPayload,
+    @Param('scheduleId') scheduleId: number,
+  ) {
+    return this.taskService.findTaskBySchedule(scheduleId, userId);
   }
 }
