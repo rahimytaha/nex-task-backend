d03075632d4388268b3c3b88075c82a8a862098d
TAHA RAHIMY <rahimytaha9@gmail.com>
2025-12-19 19:43:53 +0330

    find all and find by id added in service task

--- src/task/task.service.ts
+++ src/task/task.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { TaskEntity } from './entity/task.entity';
 import { Repository } from 'typeorm';
@@ -11,7 +11,19 @@ export class TaskService {
     private taskRepository: Repository<TaskEntity>,
     @InjectRepository(CheckTaskEntity)
     private checkTaskEntity: Repository<CheckTaskEntity>,
-  ) {
-    
+  ) {}
+  async findAllTask(): Promise<TaskEntity[]> {
+    return this.taskRepository.find();
+  }
+  async findTaskById(id: number, userId?: number): Promise<TaskEntity> {
+    const existTask = await this.taskRepository.findOneBy({
+      id,
+      schedule: { user: { id: userId } },
+    });
+    if (!existTask)
+      throw new NotFoundException(
+        'task could not found or this task is not for this userId',
+      );
+    return existTask;
   }
 }
