0690a480be3ccea80a38125eed6db916d33dfe4c
TAHA RAHIMY <rahimytaha9@gmail.com>
2025-12-19 20:22:57 +0330

    task dto updated

--- src/task/dto/createTask.dto.ts
+++ src/task/dto/createTask.dto.ts
@@ -1,11 +1,14 @@
-import { IsNumber, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsNumber, IsOptional, IsString } from "class-validator";
 
 export class CreateTaskDto{
+    @IsNotEmpty()
     @IsString()
     name:string
+    @IsNotEmpty()
     @IsString()
     @IsOptional()
     description?:string
     @IsNumber()
+    @IsNotEmpty()
     scheduleId:number
 }
