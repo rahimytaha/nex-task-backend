caa7ff310640013493cb1a05f233778d2cb06ff5
TAHA RAHIMY <rahimytaha9@gmail.com>
2025-12-19 15:02:13 +0330

    schedule service done

--- src/schedule/schedule.service.ts
+++ src/schedule/schedule.service.ts
@@ -1,4 +1,4 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ScheduleEntity } from './entity/schedule.entity';
 import { Repository } from 'typeorm';
@@ -15,8 +15,38 @@ export class ScheduleService {
     const data = await this.scheduleRepository.find();
     return data;
   }
-  async create(dto:CreateScheduleDto){}
-  async update(dto:UpdateScheduleDto){}
-  async delete(id:UpdateScheduleDto){}
-  
+  async create(dto: CreateScheduleDto, userId: number) {
+    const newSchedule = new ScheduleEntity();
+    newSchedule.save({ data: { ...dto, userId } });
+    return newSchedule.id;
+  }
+  async update(dto: UpdateScheduleDto, userId: number) {
+    const existSchedule = await this.scheduleRepository.findOne({
+      where: { user: { id: userId } },
+    });
+    if (!existSchedule)
+      throw new NotFoundException(
+        'schedule could not found or this schedule is not for this userId',
+      );
+    existSchedule.save({ data: dto });
+    return existSchedule.id;
+  }
+  async delete(id: number) {
+    const existSchedule = await this.scheduleRepository.findOne({
+      where: { id },
+    });
+    if (!existSchedule)
+      throw new NotFoundException(
+        'schedule could not found or this schedule is not for this userId',
+      );
+    existSchedule.remove();
+    return true;
+  }
+  async findByUserId(userId: number) {
+    const existSchedule = await this.scheduleRepository.find({
+      where: { user: { id: userId } },
+    });
+    return existSchedule
+  }
+  async findById(id: number) {}
 }
